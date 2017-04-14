dc486b2a2c24ae93467ee0f8500b9a19f2d4cb2d
Daisuke Sakamoto <daisuke.sakamoto@mixi.co.jp>
2017-04-14 11:21:05 +0900

    greeeeeeeen!!!!!

---
+++ lib/empty_stack_exception.rb
@@ -0,0 +1,1 @@
+class EmptyStackException < StandardError; end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,2 +1,34 @@
+require_relative './empty_stack_exception'
+
 class Stack
+  def initialize
+    @stack = []
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def push(obj)
+    @stack << obj
+  end
+
+  def size
+    @stack.size
+  end
+
+  def top
+    if @stack.size == 0
+      raise EmptyStackException
+    end
+    @stack[0]
+  end
+
+  def pop
+    if @stack.size == 0
+      raise EmptyStackException
+    end
+
+    @stack.pop
+  end
 end
